710c1503efdc2d109538f0c33383140a69f6233b
Abdul <abdulganem@gmail.com>
2020-04-26 20:28:39 -0300

    agregoCargaDeListaNegra

--- src/main/java/seguridad/CriterioFueraListaNegra.java
+++ src/main/java/seguridad/CriterioFueraListaNegra.java
@@ -1,16 +1,33 @@
 package seguridad;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CriterioFueraListaNegra implements CriterioValidacion {
 
 	List<String> listaNegra;
+	String archivo; // ruta de la lista negra
 
 	public CriterioFueraListaNegra(){
 		this.listaNegra = new ArrayList<String>();
 		listaNegra.add("123456789");
 	}
+	
+	//Agrego la lista negra
+	public  void agregarListaNegra(String archivo) throws FileNotFoundException, IOException {
+        String cadena;
+        FileReader f = new FileReader(archivo);
+        BufferedReader b = new BufferedReader(f);
+        while((cadena = b.readLine())!=null) {
+        	this.listaNegra.add(cadena);
+        }
+        b.close();
+    }
+
 
 	@Override
 	public void validar(Usuario usuario, List<String> mensajesDeError) {
@@ -19,3 +36,5 @@ public class CriterioFueraListaNegra implements CriterioValidacion {
 		}
 	}
 }
+
+
