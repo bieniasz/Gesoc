7cef7ec28ebf3c64b98c4e233a96be69de88bda5
ignabenassi <ignaciobenassi@gmail.com>
2020-10-25 02:10:57 -0300

    agrego el metodo para el login del lado del controller y ajax para el manejo de request response del login.

--- GeSoc/src/main/java/db/DAOs/UserDAOMemoria.java
+++ GeSoc/src/main/java/db/DAOs/UserDAOMemoria.java
@@ -9,6 +9,20 @@ import java.util.Map;
 public class UserDAOMemoria implements UserDAO {
     private Map<String, Usuario> usuarioMap = new HashMap<>();
 
+    public UserDAOMemoria(){
+
+        Usuario usuario1 = new Usuario();
+        usuario1.setUsuarioId("igna");
+        usuario1.setContrasenia("igna");
+
+        Usuario usuario2 = new Usuario();
+        usuario1.setUsuarioId("juanpa");
+        usuario1.setContrasenia("juanpa");
+
+        this.persistirUsuario(usuario1);
+        this.persistirUsuario(usuario2);
+    }
+
     @Override
     public Usuario getUsuario(String userId) {
         return this.usuarioMap.get(userId);
@@ -18,4 +32,6 @@ public class UserDAOMemoria implements UserDAO {
     public void persistirUsuario(Usuario usuario) {
         this.usuarioMap.put(usuario.getUsuario(),usuario);
     }
+
+
 }
