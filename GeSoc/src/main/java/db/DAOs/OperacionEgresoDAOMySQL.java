ca0b3b204ba7675fb6e3220ded81f471a5bb504f
Federico Caputo <caputodfederico@gmail.com>
2020-10-28 19:28:43 -0300

    Completo DAO Egreso

---
+++ GeSoc/src/main/java/db/DAOs/OperacionEgresoDAOMySQL.java
@@ -0,0 +1,24 @@
+package db.DAOs;
+
+import db.EntityManagerHelper;
+import domain.entities.operacionComercial.OperacionEgreso;
+
+public class OperacionEgresoDAOMySQL implements OperacionEgresoDAO {
+    @Override
+    public OperacionEgreso buscarOperacionEgresoPorId(Integer id) {
+        return EntityManagerHelper.getEntityManager().find(OperacionEgreso.class, id);
+    }
+
+    @Override
+    public void guardarOperacionEgreso(OperacionEgreso egreso) {
+        EntityManagerHelper.beginTransaction();
+        EntityManagerHelper.persist(egreso);
+        EntityManagerHelper.commit();
+    }
+
+    public void actualizarOperacionEgreso(OperacionEgreso egreso){
+        EntityManagerHelper.beginTransaction();
+        EntityManagerHelper.getEntityManager().merge(egreso);
+        EntityManagerHelper.commit();
+    }
+}
--- GeSoc/src/test/java/db/TestEgresoDAO.java
+++ GeSoc/src/test/java/db/TestEgresoDAO.java
@@ -1,16 +1,20 @@
 package db;
 
+import db.DAOs.OperacionEgresoDAO;
 import db.DAOs.OperacionEgresoDAOMemoria;
+import db.DAOs.OperacionEgresoDAOMySQL;
 import domain.entities.ProveedorDocComer.Proveedor;
 import domain.entities.operacionComercial.MedioDePago;
 import domain.entities.operacionComercial.OperacionEgreso;
-import domain.entities.operacionComercial.OperacionIngreso;
 import domain.entities.organizacion.EntidadJuridica;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class TestEgresoDAO {
-    private OperacionEgresoDAOMemoria egresoDAO = new OperacionEgresoDAOMemoria();
+    private OperacionEgresoDAO egresoDAO = new OperacionEgresoDAOMemoria();
+    private int egresoId;
 
     @Test
     public void TestPersistirEgreso(){
@@ -25,12 +29,45 @@ public class TestEgresoDAO {
     }
 
     @Test
-    public void TestRecuperarEgreso() {
-        OperacionEgreso egresoRecuperado = egresoDAO.buscarEgreso(1);
+    public void TestRecuperarEgreso() throws Exception {
+        OperacionEgreso egresoRecuperado = egresoDAO.buscarOperacionEgresoPorId(1);
 
         Assert.assertNotNull(egresoRecuperado);
         Assert.assertEquals("Numero 3", egresoRecuperado.getNumeroIdentificadorMedioPago());
 
     }
 
+    @Test
+    public void TestPersistirLuegoBuscarEgresoMySQL() throws Exception {
+        egresoDAO = new OperacionEgresoDAOMySQL();
+        LocalDate fechaHoy = LocalDate.of(2020, 10, 28);
+
+        OperacionEgreso egreso = new OperacionEgreso();
+        Assert.assertEquals(0, egreso.getId());
+
+        egreso.setMedioDePago(new MedioDePago());
+        egreso.setFecha(fechaHoy);
+        egreso.setOrganizacion(new EntidadJuridica());
+        egreso.setNumeroIdentificadorMedioPago("Numero 3");
+        egreso.setProveedor(new Proveedor());
+
+        egreso.setCantidadEsperadaPresupuestos(3);
+        egresoDAO.guardarOperacionEgreso(egreso);
+
+        /*
+        Estas lineas funcionan de dos maneras
+        Primero testeando la transaccion de guardado, porque al hacer el commit,
+            guarda el id generado en el objeto
+        Segundo asegurando que ese id queda luego parametrizado por this.egresoId,
+            para no tener que adivinar el id al recuperarlo desde la base
+        */
+        this.egresoId = egreso.getId();
+        Assert.assertNotEquals(0, this.egresoId);
+
+        //Luego hago la busqueda en el mismo scope, para no perder el dato del egresoId
+        OperacionEgreso egresoRecuperado = egresoDAO.buscarOperacionEgresoPorId(this.egresoId);
+        Assert.assertNotNull(egresoRecuperado);
+        Assert.assertEquals("Numero 3", egresoRecuperado.getNumeroIdentificadorMedioPago());
+        Assert.assertEquals(fechaHoy, egresoRecuperado.getFecha());
+    }
 }
