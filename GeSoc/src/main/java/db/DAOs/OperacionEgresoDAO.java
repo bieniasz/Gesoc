52d437e4bfce246e2fa83e4924a749d632125e4c
Federico Caputo <caputodfederico@gmail.com>
2020-10-28 21:32:43 -0300

    Corrijo intentosFallidos

--- GeSoc/src/main/java/db/DAOs/OperacionEgresoDAO.java
+++ GeSoc/src/main/java/db/DAOs/OperacionEgresoDAO.java
@@ -1,14 +1,9 @@
 package db.DAOs;
 
 import domain.entities.operacionComercial.OperacionEgreso;
-import domain.entities.operacionComercial.OperacionIngreso;
 
 public interface OperacionEgresoDAO {
 
     void guardarOperacionEgreso(OperacionEgreso operacion);
-
     OperacionEgreso buscarOperacionEgresoPorId(Integer id) throws Exception;
-
-    public OperacionEgreso buscarEgreso(Integer id);
-
 }
