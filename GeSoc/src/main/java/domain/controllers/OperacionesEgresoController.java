4d6ef3296f319b3758b58ae5ede96eca96aec316
JuanPabloCenturion <juanpablocenturion@yahoo.com.ar>
2020-10-29 18:17:28 -0300

    Canectados daos mysql a vista de todos los egresos

--- GeSoc/src/main/java/domain/controllers/OperacionesEgresoController.java
+++ GeSoc/src/main/java/domain/controllers/OperacionesEgresoController.java
@@ -3,24 +3,27 @@ package domain.controllers;
 import db.DAOs.*;
 import domain.entities.operacionComercial.CategoriaDeOperaciones;
 import domain.entities.operacionComercial.OperacionEgreso;
+import domain.entities.usuario.Usuario;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class OperacionesEgresoController {
     private OperacionEgresoDAO operacionEgresoDAO = new OperacionEgresoDAOMySQL();
-    private CategoriaDAO categoriaDAO = new CategoriaDAOMemoria();
+    private CategoriaDAO categoriaDAO = new CategoriaDAOMySQL();
+    private UserDAO userDAO = new UserDAOMySQL();
 
     public ModelAndView mostrarEgresos(Request request, Response response)  throws Exception {
 
         String usuarioID = request.queryParams("usuarioId");
-
-        List<OperacionEgreso> egresos = new OperacionEgresoDAOMemoria().getOperacionesEgreso();
-
+        //Usuario usuario = this.userDAO.buscarUsuarioPoruserId(usuarioID);
+        Usuario usuario = this.userDAO.buscarUsuario(new Integer(usuarioID));
+        List<OperacionEgreso> egresos = this.operacionEgresoDAO.getOperacionesEgresoPorOrganizacion(usuario.getRol().getOrganizacion().getId());
         List<CategoriaDeOperaciones> categorias = this.categoriaDAO.getTodasLasCategorias();
 
         Map<String, Object> parametros = new HashMap<>();
