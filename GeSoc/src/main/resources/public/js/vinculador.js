39a5887b26916e01122cc748a395b62450eba1da
JuanPabloCenturion <juanpablocenturion@yahoo.com.ar>
2020-11-11 16:58:04 -0300

    FE vinculador

---
+++ GeSoc/src/main/resources/public/js/vinculador.js
@@ -0,0 +1,54 @@
+function mostrarVinculadorModal() {
+    document.getElementById("modalAgregarDetalle").style.display = 'block';
+}
+
+/*
+  <div id="divMensajesDelVinculador"></div>
+
+  <div class="alert">
+      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+      <strong>Vinculacion en proceso</strong>
+  </div>
+*/
+
+function correrVinculacion() {
+    var div = document.createElement("DIV");
+        div.setAttribute("class", "alert");
+        document.getElementById("divMensajesDelVinculador").appendChild(div);
+
+    var span = document.createElement("SPAN");
+        span.setAttribute("class", "closebtn");
+        span.setAttribute("onclick", "this.parentElement.style.display='none';");
+        span.innerHTML = "&times;";
+        div.appendChild(span);
+
+    var strong = document.createElement("STRONG");
+        strong.innerHTML = "Vinculacion en proceso";
+        div.appendChild(strong);
+
+    document.getElementById("modalAgregarDetalle").style.display = 'none';
+/*
+$.post( "/vincular", {
+        criterio: document.getElementById("criterioTipo").value
+    } ).done( mensajeComplete() ); */
+}
+
+function mensajeComplete() {
+    var div = document.createElement("DIV");
+        div.setAttribute("class", "alert success");
+        document.getElementById("divMensajesDelVinculador").appendChild(div);
+
+    var span = document.createElement("SPAN");
+        span.setAttribute("class", "closebtn");
+        span.setAttribute("onclick", "this.parentElement.style.display='none';");
+        span.innerHTML = "&times;";
+        div.appendChild(span);
+
+    var strong = document.createElement("STRONG");
+        strong.innerHTML = "Vinculacion completada";
+        div.appendChild(strong);
+}
+
+function cerrarVinculadorModal() {
+    document.getElementById("modalAgregarDetalle").style.display = 'none';
+}
