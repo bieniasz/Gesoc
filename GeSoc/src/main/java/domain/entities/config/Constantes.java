3c2983c19202d6d9974cdf3a9a313d9951f974d9
Federico Caputo <caputo.d.federico@gmail.com>
2020-11-11 11:59:44 -0300

    Subo archivos de constantes para usar en el sistema. Tiene los string de criterios del vinculador

---
+++ GeSoc/src/main/java/domain/entities/config/Constantes.java
@@ -0,0 +1,9 @@
+package domain.entities.config;
+
+public class Constantes {
+    /* Criterios de ordenamiento del Vinculador */
+    public static final String vinculador_criterios_PrimeroEgreso = "Orden-Valor-PrimeroEgreso";
+    public static final String vinculador_criterios_PrimeroIngreso = "Orden-Valor-PrimeroIngreso";
+    public static final String vinculador_criterios_Fecha = "Fecha";
+    public static final String vinculador_criterios_Mix = "Mix";
+}
