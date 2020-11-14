30db960fe79a7c1ecdba08400ed3d9ed3e4ead93
Federico Caputo <caputo.d.federico@gmail.com>
2020-11-14 01:08:35 -0300

    Vinculador. Corrijo modal de selección de Criterio en pantalla de Ingresos

--- GeSoc/src/main/resources/public/js/vinculador.js
+++ GeSoc/src/main/resources/public/js/vinculador.js
@@ -1,23 +1,58 @@
+var criteriosAdicionales;
+
 function mostrarVinculadorModal() {
-    document.getElementById("modalAgregarDetalle").style.display = 'block';
+    resetBusquedaMix();
+    $("#modalVinculador").show();
 }
 
-var criteriosAdicionales = [];
+function onCriterioSelected() {
+    var criterio = $("#criterioTipo").val();
+    if(criterio == "Mix") {
+        $("#botonBusquedaMixHidden").removeClass("btn-search-hidden");
+    } else {
+        resetBusquedaMix();
+    }
+}
+
+function resetBusquedaMix() {
+    criteriosAdicionales = [];
+    $("#botonBusquedaMixHidden").addClass("btn-search-hidden");
+    $("#criteriosExtra").empty();
+    $("#buscarMix a").removeClass("critSeleccionado");
+    $("#buscarMix a").hide();
+    $("#buscarMix").hide();
+}
+
+function toggleMixDropdown() {
+    $criteriosDisponibles = $("#buscarMix a").filter(function(i){ return !$(this).hasClass("critSeleccionado"); });
+    $criteriosDisponibles.show();
+    $("#buscarMix").toggle();
+}
+
 function agregarCriterioExtra(criterioElegido) {
-
     criteriosAdicionales.push(criterioElegido);
-
-    var criterio = document.createElement("SPAN");
+    $elegido = $("#buscarMix a:contains('" + criterioElegido + "')");
+    $elegido.addClass("critSeleccionado");
+    $elegido.hide();
+    var criterio = document.createElement("span");
         criterio.setAttribute("class", "label label-info");
         criterio.innerHTML = criterioElegido;
-        document.getElementById("criteriosExtra").appendChild(criterio);
+/*
+    var span = document.createElement("span");
+        span.setAttribute("class", "closebtn");
+        span.setAttribute("onclick", "this.parentElement.style.display='none';");
+        span.innerHTML = "&times;";
+
+    criterio.appendChild(span);*/
+    document.getElementById("criteriosExtra").appendChild(criterio);
 
     console.log(criteriosAdicionales);
 }
 
+
 function correrVinculacion(organizacion) {
     var div = document.createElement("DIV");
-        div.setAttribute("class", "alert");
+        div.setAttribute("class", "alert alert-progress");
         document.getElementById("divMensajesDelVinculador").appendChild(div);
 
     var span = document.createElement("SPAN");
@@ -30,7 +65,7 @@ function correrVinculacion(organizacion) {
         strong.innerHTML = "Vinculacion en proceso";
         div.appendChild(strong);
 
-    document.getElementById("modalAgregarDetalle").style.display = 'none';
+    document.getElementById("modalVinculador").style.display = 'none';
 
     $.post( "/vincular", {
         criterio: document.getElementById("criterioTipo").value,
@@ -40,8 +75,11 @@ function correrVinculacion(organizacion) {
 }
 
 function mensajeComplete() {
+    var $divAlert = $(".alert-progress");
+        $divAlert.hide();
+
     var div = document.createElement("DIV");
-        div.setAttribute("class", "alert success");
+        div.setAttribute("class", "alert alert-success");
         document.getElementById("divMensajesDelVinculador").appendChild(div);
 
     var span = document.createElement("SPAN");
@@ -56,18 +94,7 @@ function mensajeComplete() {
 }
 
 function cerrarVinculadorModal() {
-    document.getElementById("modalAgregarDetalle").style.display = 'none';
+    $("#criterioTipo option:contains('Egreso')").prop('selected',true);
+    resetBusquedaMix();
+    $("#modalVinculador").hide();
 }
-
-function mostrarMixBusqueda() {
-    var criterio = document.getElementById("criterioTipo").value;
-
-    if(criterio == "Mix") {
-        document.getElementById("botonBusquedaMixHidden").style.visibility = 'visible';
-    }
-}
-
-function mostrarMixDropdown() {
-    document.getElementById("buscarMix").classList.toggle("show");
-}
-
