e224d97de8c937597555ade00cf74aa4e928bd90
JuanPabloCenturion <juanpablocenturion@yahoo.com.ar>
2020-11-16 14:05:23 -0300

    Mensaje de documento guardado

--- GeSoc/src/main/resources/public/js/documentoComercial.js
+++ GeSoc/src/main/resources/public/js/documentoComercial.js
@@ -97,7 +97,7 @@ function postDocumentoDigital(idEgreso) {
                         tipoComprobanteId: document.getElementById("documentoComercialTipoComprobanteId").value,
                         contenidoSerializado: reader.result
                     } ).done( function() {
-                            //location.reload(true);
+                            mensajeDocumentoAdjuntado();
                     });
        };
        reader.onerror = function (error) {
@@ -114,7 +114,7 @@ function postDocumentoFisico(idEgreso) {
             tipoComprobanteId: document.getElementById("documentoComercialTipoComprobanteId").value,
             contenidoSerializado: ""
         } ).done( function() {
-                //location.reload(true);
+                mensajeDocumentoAdjuntado();
         });
 }
 
@@ -140,7 +140,7 @@ function postDocumentoDigitalPresupuesto(idEgreso) {
                            tipoComprobanteId: document.getElementById("documentoComercialTipoComprobanteId").value,
                            contenidoSerializado: reader.result
                        } ).done( function() {
-                               location.reload(true);
+                               mensajeDocumentoAdjuntado();
                        });
           };
           reader.onerror = function (error) {
@@ -157,7 +157,7 @@ function postDocumentoFisicoPresupuesto(idEgreso) {
             tipoComprobanteId: document.getElementById("documentoComercialTipoComprobanteId").value,
             contenidoSerializado: ""
         } ).done( function() {
-                location.reload(true);
+                mensajeDocumentoAdjuntado();
         });
 }
 
@@ -185,4 +185,24 @@ function llenarTipoComprobante(id, descripcion) {
     document.getElementById("documentoComercialTipoComprobanteId").value = id;
 
     document.getElementById("buscarDocumentoComercialTipoComprobante").classList.toggle("show");
+}
+
+function mensajeDocumentoAdjuntado() {
+    setTimeout(function() {
+        document.getElementById("modalAgregarDetalle").style.display = 'none';
+
+        var div = document.createElement("DIV");
+            div.setAttribute("class", "alert alert-success");
+            document.getElementById("divMensajesDocumentoGuardado").appendChild(div);
+
+        var span = document.createElement("SPAN");
+            span.setAttribute("class", "closebtn");
+            span.setAttribute("onclick", "this.parentElement.style.display='none';");
+            span.innerHTML = "&times;";
+            div.appendChild(span);
+
+        var strong = document.createElement("STRONG");
+            strong.innerHTML = "Documento guardado";
+            div.appendChild(strong);
+    }, 4000);
 }
